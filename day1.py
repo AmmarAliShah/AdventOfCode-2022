65e9b0531104b6478f233d1689a79e2d5f718d9d
Ammar Shah <syedammarali24@gmail.com>
2022-12-13 01:02:16 +0500

    Day 1 - first half

--- day1.py
+++ day1.py
@@ -1,3 +1,15 @@
+sum = 0;
+max = 0;
+
 with open('day1-input.txt', 'r') as f:
-    [print(line) for line in f.readlines()]
+    for line in f.readlines():
+        if (line != '\n'):
+            sum += int(line.strip());
+            #print(type(int(line)));
+        else:
+            if (max < sum):
+                max = sum;
+            sum = 0;
     f.close();
+
+print(max);
