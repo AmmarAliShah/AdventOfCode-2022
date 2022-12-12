17ea61eed4dee7cadda645b64c628b5fc2536485
Ammar Shah <syedammarali24@gmail.com>
2022-12-13 01:13:26 +0500

    Day 1 - Part 2

--- day1.py
+++ day1.py
@@ -1,15 +1,23 @@
 sum = 0;
 max = 0;
+totals = [];
 
+
+# Itterate through file input and save sums to a array
 with open('day1-input.txt', 'r') as f:
     for line in f.readlines():
         if (line != '\n'):
             sum += int(line.strip());
-            #print(type(int(line)));
         else:
-            if (max < sum):
-                max = sum;
+            totals.append(sum);
+            #if (max < sum):
+            #    max = sum;
             sum = 0;
     f.close();
 
-print(max);
+# Sort the array
+totals.sort(reverse=True);
+
+# Add top 3 and print
+total = totals[0] + totals[1] + totals[2]
+print(total);
