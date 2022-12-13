a6cff9afb0474b0e6d1e861b3fb9bbffde549b4b
Ammar Shah <syedammarali24@gmail.com>
2022-12-13 10:26:49 +0500

    Day 3 - Part 1

--- day3.py
+++ day3.py
@@ -1,21 +1,23 @@
-sum = 0;
 priority = 0;
 
 with open('day3-input.txt', 'r') as f:
     for line in f.readlines():
+        oldItemCode = 0;
         items = list(line.strip());
         comp1 = items[:len(items)//2]
         comp2 = items[len(items)//2:]
         
         for i in range(len(comp1)):
-            if comp1[i] == comp2[i]:
-                itemCode = ord(comp1[i]);
-                if itemCode >= 65 and itemCode <= 90:
-                    priority += (itemCode - 38);
-                elif itemCode >= 97 and itemCode <= 122:
-                    priority += (itemCode - 96);
-        print(comp1[i]);
+            for j in range(len(comp2)):
+                if comp1[i] == comp2[j]:
+                    #print(comp1[i]);
+                    itemCode = ord(comp1[i]);
+                    if itemCode != oldItemCode:
+                        if itemCode >= 65 and itemCode <= 90:
+                            priority += (itemCode - 38);
+                        elif itemCode >= 97 and itemCode <= 122:
+                            priority += (itemCode - 96);
+                        oldItemCode = itemCode;
         #print("Compartment1: " + str(comp1) + "\nCompartment2: " + str(comp2));
-    
 f.close();
-#print(priority);
+print(priority);
